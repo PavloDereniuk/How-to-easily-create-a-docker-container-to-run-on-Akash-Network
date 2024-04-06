db53594fd954dafa8c3f8781648d9e7b9fa7d205
PavloDereniuk <125708520+PavloDereniuk@users.noreply.github.com>
2024-04-07 00:16:48 +0300

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '../dist/vite.svg'
-import './App.css'
+import akashLogo from "./assets/akash-red.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={akashLogo} className="logo react" alt="Akash Network" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <a href="https://akash.network/" target="_blank">
+        <h1>Discover Akash Network</h1>
+      </a>
+      <div className="card"></div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
